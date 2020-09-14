2e5fd768691129a340a59cdf438572ca1f4887f5
Konstantin Troshin <k.troshin@fme.de>
2020-09-14 20:20:00 +0200

    Added loop for nginx

--- bootstrap/nginx.sh
+++ bootstrap/nginx.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 curl -L -o nginx.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/aws/deploy.yaml
-kubectl apply -f nginx.yaml
+cnt=1
+while true
+do 
+  kubectl apply -f nginx.yaml
+  if [[ $? != 0 ]]
+  then
+    if [[ $cnt -gt 10 ]]
+    then
+      break
+    else
+      cnt=$(($cnt+1))
+      echo "Deployment of nginx failed, $cnt attempt coming ..."
+      sleep 10
+    fi
+  else
+    break
+  fi
+done
