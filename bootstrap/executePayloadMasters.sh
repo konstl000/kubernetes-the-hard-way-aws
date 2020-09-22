1c464ae7531f4bb0cb561d88512b77a8eb035dba
Konstantin Troshin <k.troshin@fme.de>
2020-09-22 11:59:18 +0200

    Moved execute payload routine to a separate script, added multiple attempts for payload execution

--- bootstrap/executePayloadMasters.sh
+++ bootstrap/executePayloadMasters.sh
@@ -1,95 +1,4 @@
 #!/bin/bash
-function prepareAwsCni(){
-  curl -LO https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/master/config/v1.6/aws-k8s-cni.yaml
-  sed -i 's/us-west-2/'$(aws configure get region)'/g' aws-k8s-cni.yaml
-  kubectl apply -f aws-k8s-cni.yaml
-}
-function getInstancesByTag () {
- aws ec2 describe-instances --filter "Name=tag-value,Values=$1" | jq -c '.Reservations[].Instances[] | select (.State.Name == "running")'
-}
-function getInstanceId(){
- echo "$1" | sed -n "$2p" | jq -r '.InstanceId'
-}
-function copyToInstance(){
-  rsync -rv -e "ssh -i ../rsa/k8s.pem -o StrictHostKeyChecking=no" "$1" 'ubuntu@'"$(getInstanceId "$2" "$3")"':'"$4"
-}
-function getPrivateIp(){
-  echo "$1" | sed -n "$2p" | jq -r '.PrivateIpAddress'
-}
-function getPrivateDnsName(){
-  echo "$1" | sed -n "$2p" | jq -r '.PrivateDnsName'
-}
-
-masters=$(getInstancesByTag k8smaster)
-workers=$(getInstancesByTag k8snode)
-function connectAndExecute(){
-      if [[ -z "$DRY_RUN" ]] || [[ "$DRY_RUN" == "" ]]
-      then
-      echo "connecting to $(getInstanceId "$2" "$3")"
-      ssh -tt -i ../rsa/k8s.pem -o StrictHostKeyChecking=no -l ${USERNAME} $(getInstanceId "$2" "$3") <<EOF | tee protocol.txt
-${1}
-exit
-EOF
-else
-  echo -e "PAYLOAD:\n"
-  echo "$1"
-fi
-if [[ "$4" != "" ]] && [[ "$4" != 0 ]]
-then
-  local payload=$(preparePayload "$(echo "{\"payload\":\"handleBackgroundScript.sh\"}")")
-  connectAndExecute "$payload" "$2" "$3"
-fi
-}
-function preparePayload(){
-  local content=$(cat "$(echo "$1" | jq -r '.payload')")
-  varlines=$(echo -e "$content" | grep '\${' )
-  while read -r varline
-  do
-    if [[ "$varline" != "" ]]
-    then
-      local varnames=$(echo "$varnames $(echo $varline | perl -ne 'print "$1 " while /(\$\{.*?\})/g')")
-    fi
-  done<<<"$varlines"
-  for varname in $varnames
-  do
-    content=$(echo "$content" | sed 's`'"$varname"'`'"$(eval echo "$varname")"'`g')
-  done
-  if [[ $(echo "$1" | jq -r '.background') == "" ]] || [[ $(echo "$1" | jq -r '.background') == null ]] 
-  then
-   echo '
-    while true
-    do
-      name=$(dd if=/dev/urandom count=32 bs=1 | base64 -w0 | sed -e "s/[^[:alnum:]]//g" -e "s/$/.sh/")
-      if [[ "$(ls $name 2>/dev/null)" == "" ]]
-      then
-        break
-      fi
-    done
-'
-  echo "echo '$(echo "$content" | base64)' | base64 -d > \$name"
-  echo 'chmod +x $name'
-  echo './$name'
-  echo 'rm -f ./$name'
-  else
-    echo '
-    while true
-    do
-      name=$(dd if=/dev/urandom count=32 bs=1 | base64 -w0 | sed -e "s/[^[:alnum:]]//g" -e "s/$//")
-      if [[ "$(ls $name 2>/dev/null)" == "" ]]
-      then
-        break
-      fi
-    done
-    '
-    echo 'mkdir $name'
-    echo "echo '$(echo "$content" | base64)' | base64 -d > \$name/$(echo "$1" | jq -r '.script_name')"
-    echo "chmod +x \$name/$(echo "$1" | jq -r '.script_name')"
-    echo "./\$name/$(echo "$1" | jq -r '.script_name') >> ./\$name/exec.log 2>> ./\$name/exec.log &"
-  fi
-}
-function getScriptName(){
-  SCRIPT_NAME="$(dd if=/dev/urandom count=32 bs=1 | base64 -w0 | sed -e "s/[^[:alnum:]]//g" -e "s/$/.sh/")"
-}
 function main(){
 for index in 1 2 3; do
   for filename in etcd.sh
@@ -109,9 +18,11 @@ for index in 1 2 3; do
   done
 done
 }
+source ./executePayload.sh
 USERNAME=ubuntu
 CONTROLLER_CONFIG=""
 ETCD_SERVERS=""
+masters=$(getInstancesByTag k8smaster)
 for index in 1 2 3; do
   ETCD_SERVERS=$(echo "$ETCD_SERVERS,https://$(getPrivateIp "$masters" "$index"):2379")
   CONTROLLER_CONFIG=$(echo "$CONTROLLER_CONFIG,$(getPrivateDnsName "$masters" "$index")=https://$(getPrivateIp "$masters" "$index"):2380")
--- bootstrap/executePayloadWorkers.sh
+++ bootstrap/executePayloadWorkers.sh
@@ -8,92 +8,6 @@ function prepareAwsCni(){
   sed -i 's/us-west-2/'"$AWS_DEFAULT_REGION"'/g' aws-k8s-cni.yaml
   kubectl apply -f aws-k8s-cni.yaml
 }
-function getInstancesByTag () {
- aws ec2 describe-instances --filter "Name=tag-value,Values=$1" | jq -c '.Reservations[].Instances[] | select (.State.Name == "running")'
-}
-function getInstanceId(){
- echo "$1" | sed -n "$2p" | jq -r '.InstanceId'
-}
-function copyToInstance(){
-  rsync -rv -e "ssh -i ../rsa/k8s.pem -o StrictHostKeyChecking=no" "$1" 'ubuntu@'"$(getInstanceId "$2" "$3")"':'"$4"
-}
-function getPrivateIp(){
-  echo "$1" | sed -n "$2p" | jq -r '.PrivateIpAddress'
-}
-function getPrivateDnsName(){
-  echo "$1" | sed -n "$2p" | jq -r '.PrivateDnsName'
-}
-
-masters=$(getInstancesByTag k8smaster)
-workers=$(getInstancesByTag k8snode)
-function connectAndExecute(){
-      if [[ -z "$DRY_RUN" ]] || [[ "$DRY_RUN" == "" ]]
-      then
-      echo "connecting to $(getInstanceId "$2" "$3")"
-      ssh -tt -i ../rsa/k8s.pem -o StrictHostKeyChecking=no -l ${USERNAME} $(getInstanceId "$2" "$3") <<EOF | tee protocol.txt
-${1}
-exit
-EOF
-else
-  echo -e "PAYLOAD:\n"
-  echo "$1"
-fi
-if [[ "$4" != "" ]] && [[ "$4" != 0 ]]
-then
-  local payload=$(preparePayload "$(echo "{\"payload\":\"handleBackgroundScript.sh\"}")")
-  connectAndExecute "$payload" "$2" "$3"
-fi
-}
-function preparePayload(){
-  local content=$(cat "$(echo "$1" | jq -r '.payload')")
-  varlines=$(echo -e "$content" | grep '\${' )
-  while read -r varline
-  do
-    if [[ "$varline" != "" ]]
-    then
-      local varnames=$(echo "$varnames $(echo $varline | perl -ne 'print "$1 " while /(\$\{.*?\})/g')")
-    fi
-  done<<<"$varlines"
-  for varname in $varnames
-  do
-    content=$(echo "$content" | sed 's`'"$varname"'`'"$(eval echo "$varname")"'`g')
-  done
-  if [[ $(echo "$1" | jq -r '.background') == "" ]] || [[ $(echo "$1" | jq -r '.background') == null ]] 
-  then
-   echo '
-    while true
-    do
-      name=$(dd if=/dev/urandom count=32 bs=1 | base64 -w0 | sed -e "s/[^[:alnum:]]//g" -e "s/$/.sh/")
-      if [[ "$(ls $name 2>/dev/null)" == "" ]]
-      then
-        break
-      fi
-    done
-'
-  echo "echo '$(echo "$content" | base64)' | base64 -d > \$name"
-  echo 'chmod +x $name'
-  echo './$name'
-  echo 'rm -f ./$name'
-  else
-    echo '
-    while true
-    do
-      name=$(dd if=/dev/urandom count=32 bs=1 | base64 -w0 | sed -e "s/[^[:alnum:]]//g" -e "s/$//")
-      if [[ "$(ls $name 2>/dev/null)" == "" ]]
-      then
-        break
-      fi
-    done
-    '
-    echo 'mkdir $name'
-    echo "echo '$(echo "$content" | base64)' | base64 -d > \$name/$(echo "$1" | jq -r '.script_name')"
-    echo "chmod +x \$name/$(echo "$1" | jq -r '.script_name')"
-    echo "./\$name/$(echo "$1" | jq -r '.script_name') >> ./\$name/exec.log 2>> ./\$name/exec.log &"
-  fi
-}
-function getScriptName(){
-  SCRIPT_NAME="$(dd if=/dev/urandom count=32 bs=1 | base64 -w0 | sed -e "s/[^[:alnum:]]//g" -e "s/$/.sh/")"
-}
 function main(){
 prepareAwsCni
 for index in 1 2 3; do
@@ -110,5 +24,7 @@ for index in 1 2 3; do
   done
 done
 }
+source ./executePayload.sh
 USERNAME=ubuntu
+workers=$(getInstancesByTag k8snode)
 main
